868f37ff6895138986ddc1e169cc4476adf0cd8a
sp0nsor <spons0rr1703@gmail.com>
2022-05-07 01:37:19 +0300

    Solution of the sixth lab work (FAaP)

---
+++ FAaP/lab 6/main/main/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// алитплтиплимлмьалдиьплилмалмьад
+
+void main(int argc,char* argv[]) {
+
+	setlocale(LC_ALL, "Ru");
+
+	char Str1[100];
+	char Str2[100];
+	cin.getline(Str1, 100);
+	cin.getline(Str2, 100);
+
+	string S1(Str1);
+	string S2(Str2);
+	string S3 = " ";
+	string S4 = "";
+	unsigned int res;
+	unsigned int cnt;
+	unsigned int i = 0;
+	
+	res = (S1.size() + S2.size());
+
+	while (S3.size()<= res) {
+
+		S3 += string{ S1[i] } + string{ S2[i] };
+		i++;
+		
+	}
+
+
+	cout << S3;
+	
+}
