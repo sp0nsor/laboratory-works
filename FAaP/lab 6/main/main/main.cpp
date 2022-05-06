0815d10328f5a6ecc76dcc9f6363d5a75133852d
sp0nsor <spons0rr1703@gmail.com>
2022-05-07 02:40:45 +0300

    Completion of the correction conversion of a new line 'task 1'

--- FAaP/lab 6/main/main/main.cpp
+++ FAaP/lab 6/main/main/main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 using namespace std;
 
-// алитплтиплимлмьалдиьплилмалмьад
 
 void main(int argc,char* argv[]) {
 
@@ -10,7 +9,10 @@ void main(int argc,char* argv[]) {
 
 	char Str1[100];
 	char Str2[100];
+
+	cout << "enter first line ====>";
 	cin.getline(Str1, 100);
+	cout << "enter second line ===>";
 	cin.getline(Str2, 100);
 
 	string S1(Str1);
@@ -18,19 +20,40 @@ void main(int argc,char* argv[]) {
 	string S3 = " ";
 	string S4 = "";
 	unsigned int res;
-	unsigned int cnt;
+	unsigned int zxc = 0;
 	unsigned int i = 0;
+
 	
-	res = (S1.size() + S2.size());
+	if (S1.size() > S2.size()) {
 
-	while (S3.size()<= res) {
+		for (unsigned a = S2.size(); a <= S1.size(); a++) {
 
-		S3 += string{ S1[i] } + string{ S2[i] };
-		i++;
-		
+			S4 += string{ S1[a] };
+
+		}
+
+	}
+	if (S2.size() > S1.size()) {
+
+		for (unsigned a = S1.size(); a < S2.size(); a++) {
+
+			S4 += string{ S2[a] };
+
+		}
+
 	}
 
+	res = (S1.size() + S2.size()) - S4.size();
 
-	cout << S3;
+	while (S3.size()<= res) {
+		
+		S3 += string{ S1[i] } + string{ S2[i] };
+		i++;
+
+	}
+
+	cout << "new line: " << S3 + S4;
+
+	system("pause>>void");
 	
 }
