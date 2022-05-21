c9b765d2658dd8bc887d44197983487306dba259
sp0nsor <spons0rr1703@gmail.com>
2022-05-21 04:35:19 +0300

    initial lab

--- FAaP/lab 5/task 2/task 2/iomusic.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "music.h"
-
-istream& operator >> (istream& in, MUSIC& Music);
-ostream& operator << (ostream& out, MUSIC Music);
-istream& operator >> (istream& in, CATALOG& Catalog);
-ostream& operator << (ostream& out, CATALOG Catalog);
