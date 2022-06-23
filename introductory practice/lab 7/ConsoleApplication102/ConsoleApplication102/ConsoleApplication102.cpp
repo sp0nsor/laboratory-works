7b2c36d979ab28d6047fd6a3bbe4057b76ea727e
sp0nsor <spons0rr1703@gmail.com>
2022-06-23 12:31:39 +0300

    completion work

---
+++ introductory practice/lab 7/ConsoleApplication102/ConsoleApplication102/ConsoleApplication102.cpp
@@ -0,0 +1,19 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    ifstream in("file.txt"); // Открыли файл
+    ofstream out("new_sorted_file.txt");
+    string buf; // переменная, в которую будем считывать данные
+    while (!in.eof()) {
+        getline(in, buf); // Считали
+        if (buf.size() > 30)
+            out << buf << endl;
+    }
+    in.close();
+    out.close();
+    return 0;
+}
+
+
