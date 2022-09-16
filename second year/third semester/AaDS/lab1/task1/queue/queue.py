85d1975467b9cb2a9e2a589bfd2854357ae63471
sponssoorrr <spons0rr1703@gmail.com>
2022-09-16 11:46:13 +0300

    the main version of the solution

--- second year/third semester/AaDS/lab1/task1/queue/queue.py
+++ second year/third semester/AaDS/lab1/task1/queue/queue.py
@@ -8,12 +8,54 @@ class Queue:
     def pop (self):
         return self.items.pop()
 
+class Stack:
+     def __init__(self):
+         self.items = []
 
-qwe = Queue()
+     def push(self, item):
+         self.items.append(item)
 
-qwe.push(1)
-qwe.push(2)
-qwe.push(3)
-qwe.push(4)
+     def pop(self):
+         return self.items.pop()
 
-print (qwe.pop())
+class Deque:
+    def __init__(self):
+        self.items = []
+    
+    def addFront(self,item):
+        self.items.append(item)
+    
+    def addRear(self, item):
+        self.items.insert(0,item)
+    
+    def removeFront(self):
+        return self.items.pop()
+    
+    def removeRear(self):
+        return self.items.pop(0)
+
+
+queue = Queue()
+stack = Stack()
+deque = Deque()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+while (True):
+    if(len(stack.items) != 0):
+        tmp = stack.pop()
+        queue.push(tmp)
+    else:
+        break
+
+while (True):
+    if (len (queue.items) != 0):
+        tmp = queue.pop()
+        deque.addRear(tmp)
+    else:
+        break
+
+print (deque.removeFront())
+print (deque.removeRear())
