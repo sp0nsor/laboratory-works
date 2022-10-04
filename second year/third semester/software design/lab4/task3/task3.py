d9cd52de757d39024749711d473025dad7d8419c
sponssoorrr <spons0rr1703@gmail.com>
2022-10-04 23:28:10 +0300

    first version solution(SE)

---
+++ second year/third semester/software design/lab4/task3/task3.py
@@ -0,0 +1,12 @@
+def main():
+
+    s1 = input("введите первую строку >> ")
+    s2 = input("ввдеите вторую строку >> ")
+
+    res_str = zip (s1, s2)
+    
+    print (res_str)
+
+
+if __name__ == "__main__":
+    main()
