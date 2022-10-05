27fde2b774133cda9bf2dd2e7233d9ad844a5a4d
sponssoorrr <spons0rr1703@gmail.com>
2022-10-06 01:21:08 +0300

    Feat: solution of the fourth lab work (SD)

--- second year/third semester/software design/lab4/task3/task3.py
+++ second year/third semester/software design/lab4/task3/task3.py
@@ -1,12 +1,18 @@
 def main():
+    str1 = input('Введите первую строку: ')
+    str2 = input('Введите вторую строку: ')
 
-    s1 = input("введите первую строку >> ")
-    s2 = input("ввдеите вторую строку >> ")
+    result = ''
+    for i in zip(str1, str2):
+        result += i[0] + i[1]
 
-    res_str = zip (s1, s2)
-    
-    print (res_str)
+    if len(str1) < len(str2):
+        result += str2[len(str1):]
+    elif len(str2) < len(str1):
+        result += str1[len(str2):]
+
+    print(f'Исходная строка: {result}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
