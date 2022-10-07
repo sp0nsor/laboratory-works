643a0c36a87e1d4ff7a9a80987932e6245e795ba
sponssoorrr <spons0rr1703@gmail.com>
2022-10-07 11:46:22 +0300

    file fix

--- fisrst semestr/introductory practice/FAaP/lab 8/task 2/main/ioMusic.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Music.h"
-
-istream& operator >> (istream& in, MUSIC& Music);
-ostream& operator << (ostream& out, MUSIC Music);
-istream& operator >> (istream& in, CATALOG& Catalog);
-ostream& operator << (ostream& out, CATALOG Catalog);
--- fisrst semestr/introductory practice/module 1/lab 9-10/main/main/ioCountry.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "County.h"
-
-ostream& operator << (ostream& fout, City city);
-istream& operator >> (istream& fin, City& city);
-ostream& operator << (ostream& fout, Country country);
-istream& operator >> (istream& fin, Country& country);
