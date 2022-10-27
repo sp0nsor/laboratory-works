ade4e51e42f418b7873477a9d857527ab10b5ca9
t3rixis <96328390+sp0nsor@users.noreply.github.com>
2022-10-28 00:42:19 +0300

    added task 2

---
+++ second year/third semester/UID/lab5/task2/index.js
@@ -0,0 +1,10 @@
+function time_output(array){
+    for (let i; i < array.length; i++){
+        setTimeout(function(){
+            console.log(i);
+        }, i * 3000);
+    }
+}
+const array = [4,54,76,323,544,21];   
+
+time_output(array);
