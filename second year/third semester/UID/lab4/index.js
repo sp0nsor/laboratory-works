ca5b65800dc0c94f0fd13a932c2909e7b0d52d4e
t3rixis <96328390+sp0nsor@users.noreply.github.com>
2022-11-04 11:50:26 +0300

    Feat: solution of the seventh lab work

--- second year/third semester/UID/lab4/index.js
+++ second year/third semester/UID/lab4/index.js
@@ -18,22 +18,6 @@ function calculate(value) {
   }
 }
 
-function changeTheme() {
-  const theme = document.getElementById("theme");
-  setTimeout(() => {
-    toast.innerHTML = "Calculator";
-  }, 1500);
-  if (theme.getAttribute("href") === lightTheme) {
-    theme.setAttribute("href", darkTheme);
-    themeIcon.setAttribute("src", sunIcon);
-    toast.innerHTML = "Dark Mode 🌙";
-  } else {
-    theme.setAttribute("href", lightTheme);
-    themeIcon.setAttribute("src", moonIcon);
-    toast.innerHTML = "Light Mode ☀️";
-  }
-}
-
 function liveScreen(enteredValue) {
   if (!res.value) {
     res.value = "";
