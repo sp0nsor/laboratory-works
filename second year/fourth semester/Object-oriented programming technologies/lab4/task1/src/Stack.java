0d747f8fd129e38bbd826e1f6fb4c91a4d6fe81e
t3rixis <spons0rr1703@gmail.com>
2023-05-04 14:54:20 +0300

    Completion task2

---
+++ second year/fourth semester/Object-oriented programming technologies/lab4/task1/src/Stack.java
@@ -0,0 +1,39 @@
+public class Stack<T> {
+
+    private Node<T> top; // вершина стека
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new NullPointerException("Стек пуст");
+        }
+        T data = top.data;
+        top = top.next;
+        return data;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new NullPointerException("Стек пуст");
+        }
+        return top.data;
+    }
+}
