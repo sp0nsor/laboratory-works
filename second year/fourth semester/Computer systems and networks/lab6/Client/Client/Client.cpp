057bb04702a26db21b0bd649f8790381790ec040
t3rixis <spons0rr1703@gmail.com>
2023-06-07 00:58:24 +0300

    completion server + client

---
+++ second year/fourth semester/Computer systems and networks/lab6/Client/Client/Client.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+using namespace std;
+
+#define BUFFER_SIZE 1024
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cerr << "Ошибка инициализации Winsock." << endl;
+        return 1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        cerr << "Ошибка создания сокета." << endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    if (inet_pton(AF_INET, "127.0.0.1", &(serverAddress.sin_addr)) <= 0) {
+        cerr << "Ошибка установки IP-адреса сервера." << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+    serverAddress.sin_port = htons(17);
+
+    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        cerr << "Ошибка подключения к серверу." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Подключение к серверу установлено. Ожидание цитаты..." << endl;
+
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, sizeof(buffer));
+    if (recv(clientSocket, buffer, BUFFER_SIZE - 1, 0) == SOCKET_ERROR) {
+        cerr << "Ошибка получения цитаты." << endl;
+    }
+    else {
+        cout << "Полученная цитата: " << buffer << endl;
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ second year/fourth semester/Computer systems and networks/lab6/Server/Server/Server.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+#define BUFFER_SIZE 1024
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cerr << "Ошибка инициализации Winsock." << endl;
+        return 1;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        cerr << "Ошибка создания сокета." << endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(17);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        cerr << "Ошибка привязки сокета." << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSocket, 1) == SOCKET_ERROR) {
+        cerr << "Ошибка прослушивания сокета." << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Сервер запущен. Ожидание подключений..." << endl;
+
+    SOCKET clientSocket;
+    sockaddr_in clientAddress{};
+    int clientAddressSize = sizeof(clientAddress);
+    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressSize);
+    if (clientSocket == INVALID_SOCKET) {
+        cerr << "Ошибка принятия подключения." << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    cout << "Подключение принято. Отправка цитаты..." << endl;
+
+    const char* quote = "This is the Quote of the Day!";
+    if (send(clientSocket, quote, strlen(quote), 0) == SOCKET_ERROR) {
+        cerr << "Ошибка отправки цитаты." << endl;
+    }
+
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
