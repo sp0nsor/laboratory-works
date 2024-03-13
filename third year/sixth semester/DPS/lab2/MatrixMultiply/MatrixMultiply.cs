c106a7ad9c13443a61e5cfc4905edc78b3481113
t3rixis <spons0rr1703@gmail.com>
2024-03-13 21:10:09 +0300

    Feat: added write to file

---
+++ third year/sixth semester/DPS/lab2/MatrixMultiply/MatrixMultiply.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics;
+
+namespace DPS.Lab2.MatrixMultiply;
+
+class MatrixMultiply
+{
+    private static object _lockObject = new object();
+    private static int[][] _matrixA;
+    private static int[][] _matrixB;
+    private static int[][] _result;
+    private static Stopwatch s_stopWatch = new Stopwatch();
+    
+    private static void Main()
+    {
+        Console.WriteLine($"Введите размерность матрицы: ");
+        if (!int.TryParse(Console.ReadLine(), out int size) | size <= 1) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+
+        Console.WriteLine($"Введите количество потоков: ");
+        if (!int.TryParse(Console.ReadLine(), out int threadCount) | threadCount <= 0 | threadCount > 12)
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+
+        InitializeMatrices(size);
+        
+        Thread[] threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; i++)
+        {
+            var i1 = i;
+            threads[i] = new Thread(() => MultiplyMatrixPart(size, i1, threadCount));
+            threads[i].Start();
+        }
+        s_stopWatch.Start();
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+        s_stopWatch.Stop();
+        
+        PrintMatrixToFile(_result, "D:\\laboratory-works\\third year\\sixth semester\\DPS\\lab2\\MatrixMultiply\\result.txt");
+        Console.WriteLine($"Время выполнения операции: {s_stopWatch.Elapsed}");
+        Console.ReadLine();
+    }
+
+    private static void InitializeMatrices(int size)
+    {
+        Random random = new Random();
+
+        _matrixA = new int[size][];
+        _matrixB = new int[size][];
+        _result = new int[size][];
+
+        for (int i = 0; i < size; i++)
+        {
+            _matrixA[i] = new int [size];
+            _matrixB[i] = new int [size];
+            _result[i] = new int [size];
+
+            for (int j = 0; j < size; j++)
+            {
+                _matrixA[i][j] = random.Next(100, 10000);
+                _matrixB[i][j] = random.Next(100, 10000);
+            }
+        }
+    }
+
+    private static void MultiplyMatrixPart(int size, int threadIndex, int threadCount)
+    {
+        for (int i = threadIndex; i < size; i += threadCount)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                int particleResult = MultiplyRowColumn(_matrixA, _matrixB, i, j, size);
+                lock (_lockObject)
+                {
+                    _result[i][j] = particleResult;
+                }
+            }
+        }
+    }
+
+    private static int MultiplyRowColumn(int[][] matrixA, int[][] matrixB, int row, int column, int length)
+    {
+        int result = 0;
+        for (int k = 0; k < length; k++)
+        {
+            result += matrixA[row][k] * matrixB[k][column];
+        }
+
+        return result;
+    }
+
+    private static void PrintMatrixToFile(int[][] matrix, string filePath)
+    {
+        try
+        {
+            using (StreamWriter sw = new(filePath))
+            {
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    for (int j = 0; j < matrix[i].Length; j++)
+                    {
+                        sw.Write(matrix[i][j] + " ");
+                    }
+                    sw.WriteLine();
+                }
+                sw.Flush();
+            }
+
+            Console.WriteLine($"Результаты записаны в файл: {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при записи в файл: {ex.Message}");
+        }
+    }
+}
+
---
+++ third year/sixth semester/DPS/lab2/lab2/DoubleIntegralCalculator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace DPS.Lab2.DoubleIntegralCalculator;
+
+class DoubleIntegralCalculator
+{
+    private static object _lockObject = new object();
+    private static double _result = 0f;
+    
+    private static void Main()
+    {
+        Console.WriteLine($"Выберите функцию f(x, y): ");
+        Console.WriteLine($"1. Math.Exp(x) * Math.Sin(y)");
+        Console.WriteLine($"2. Math.Sin(x + 2 * y)");
+        Console.WriteLine($"3. Math.Cos(x*Math.Exp(y))");
+        Console.WriteLine($"4. Math.Exp(Math.Cos(x * x + y))");
+
+        if (!int.TryParse(Console.ReadLine(), out int choice) | choice > 4) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+
+        Console.WriteLine($"Введите количество потоков K:");
+        if (!int.TryParse(Console.ReadLine(), out int K) | K > 12) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+
+        Console.WriteLine($"Введите нижний предел а: ");
+        if (!double.TryParse(Console.ReadLine(), out double a)) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+
+        Console.WriteLine($"Введите верхний предел b: ");
+        if (!double.TryParse(Console.ReadLine(), out double b)) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+
+        Console.WriteLine($"Введите нижний предел с: ");
+        if (!double.TryParse(Console.ReadLine(), out double c)) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+
+        Console.WriteLine($"Введите верхний предел d: ");
+        if (!double.TryParse(Console.ReadLine(), out double d)) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+        
+        Console.Write($"Введите количество частей для разбиения отрезка [a, b] (n): ");
+        if (!int.TryParse(Console.ReadLine(), out int n)) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+        
+        Console.Write($"Введите количество частей для разбиения отрезка [c, d] (m): ");
+        if (!int.TryParse(Console.ReadLine(), out int m)) 
+            Console.Error.WriteLine("ОШИБКА: некорректные данные.");
+
+        Thread[] threads = new Thread[K];
+        Func<double, double, double> func = null;
+
+        switch (choice)
+        {
+            case 1:
+                func = (x, y) => Math.Exp(x) * Math.Sin(y);
+                break;
+            case 2:
+                func = (x, y) => Math.Sin(x + 2 * y);
+                break;
+            case 3:
+                func = (x, y) => Math.Cos(x * Math.Exp(y));
+                break;
+            case 4:
+                func = (x, y) => Math.Exp(Math.Cos(x * x + y));
+                break;
+            
+        }
+
+        for (int i = 0; i < K; i++)
+        {
+            threads[i] = new Thread(() => CalculateIntegralWithThreads(a, b, c, d, m, n, func, K, i));
+            threads[i].Start();
+        }
+
+        foreach (var thread in threads)
+            thread.Join();
+
+        Console.WriteLine($"Результат {_result}");
+        Console.ReadLine();
+    }
+
+    private static void CalculateIntegralWithThreads(double a, double b, double c, double d, int m, int n, Func<double, double, double> func, int K, int threadIndex)
+    {
+        double partialResult = 0f;
+
+        for (int i = threadIndex; i < n; i += K)
+        {
+            double x = a + i * (b - a) / n;
+            for (int j = 0; j < m; j++)
+            {
+                double y = c + j * (d - c) / m;
+                partialResult += func(x, y) * (b - a) / n * (d - c) / m;
+            }
+        }
+        lock (_lockObject)
+        {
+            _result += partialResult;
+        }
+    }
+    
+}
+
